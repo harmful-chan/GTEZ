76662edc5558a29fc154282602593f831380c028
harmful-chan <1148706823@qq.com>
2024-05-30 19:30:16 +0800

    fix(ui): 修改Dockerfile 本地构建再打包

--- wimoor-ui/vue.config.js
+++ wimoor-ui/vue.config.js
@@ -43,7 +43,7 @@ module.exports = {
         port: 8084,
         proxy: {
             '/api': { // '/api'是代理标识，用于告诉node，url前面是/api的就是使用代理的
-                target: "http://127.0.0.1:8099", //目标地址，一般是指后台服务器地址
+                target: "http://wimoor-gateway:8099", //目标地址，一般是指后台服务器地址
                 changeOrigin: false, //是否跨域
                 pathRewrite: { // pathRewrite 的作用是把实际Request Url中的'/api'用""代替
                     '^/api': ""
